ac9138342ff391fcf8bd6f18945e762685ee81bc
Muhammad Abdullah <atron1792@gmail.com>
2025-10-04 13:34:33 -0600

    ui

---
+++ scripts/UI.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _on_setting_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_resume_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
