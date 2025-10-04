04b3e644a4a58e3a1724eee142715bac84a40f37
James Wang <jwang477@mtroyal.ca>
2025-10-04 13:38:00 -0600

    Add files via upload

--- scripts/ElementScript.gd
+++ scripts/ElementScript.gd
@@ -8,7 +8,7 @@ extends Node
 # Hydrogen = 0, etc..
 # This is just shorthanding everything lol
 enum Elements {  
-    Hydrogen,
+	Hydrogen,
 	Helium,
 	Lithium,
 	Beryllium,
@@ -39,44 +39,45 @@ enum Elements {
 	Copper,
 	Zinc,
 	# Combined elements goes here (30+...)
-	Water
+	Water,
+	Plant
 }  
 
 
 
 # All recipies lol
 const RECIPES: Dictionary = {
-    "0,7": Elements.Water, # 31
+	"0,7": Elements.Water, # 31
 }
 
 const NO_COMBINATION: int = -1
-func combine_elements(element_a: Elements, element_b: Elements) -> Elements:
-    var id_a: int = element_a
-    var id_b: int = element_b
-    
+static func combine_elements(element_a: Elements, element_b: Elements) -> Elements:
+	var id_a: int = element_a
+	var id_b: int = element_b
+	
 # whatever order works
-    var key_parts: Array = [id_a, id_b]
-    key_parts.sort() 
-    
-    var combination_key: String = "%d,%d" % [key_parts[0], key_parts[1]]
-    
-    if RECIPES.has(combination_key):
-        return RECIPES[combination_key]
-    else:
-        return NO_COMBINATION
+	var key_parts: Array = [id_a, id_b]
+	key_parts.sort() 
+	
+	var combination_key: String = "%d,%d" % [key_parts[0], key_parts[1]]
+	
+	if RECIPES.has(combination_key):
+		return RECIPES[combination_key]
+	else:
+		return NO_COMBINATION
 
 
-#
-func _ready():
-    var result_water = combine_elements(Elements.Hydrogen, Elements.Oxygen)
-    print("Hydrogen + Oxygen = ", Elements.keys()[result_water])
+# remove this later
+#func _ready():
+	#var result_water = combine_elements(Elements.Hydrogen, Elements.Oxygen)
+	#print("Hydrogen + Oxygen = ", Elements.keys()[result_water])
 
-    var result_water_reverse = combine_elements(Elements.Oxygen, Elements.Hydrogen)
-    print("Oxygen + Hydrogen = ", Elements.keys()[result_water_reverse])
+	#var result_water_reverse = combine_elements(Elements.Oxygen, Elements.Hydrogen)
+	#print("Oxygen + Hydrogen = ", Elements.keys()[result_water_reverse])
 
-    var result_none = combine_elements(Elements.Helium, Elements.Neon)
-    if result_none == NO_COMBINATION:
-        print("Break")
-    
+	#var result_none = combine_elements(Elements.Helium, Elements.Neon)
+	#if result_none == NO_COMBINATION:
+	#	print("Break")
+	
 
 # Usage: var result_element = ElementScript.combine_elements(element_a, element_b)
---
+++ scripts/GameManager.gd
@@ -0,0 +1,34 @@
+# Overall game manager (controls all states), autoload this
+extends Node
+
+
+const Elements = preload("res://scripts/ElementScript.gd").Elements
+const ElementCombiner_Script = preload("res://scripts/ElementScript.gd") 
+const NO_COMBINATION = preload("res://scripts/ElementScript.gd").NO_COMBINATION
+
+var current_element_id: Elements = Elements.Oxygen # Always start as oxygen for now! Change this later on
+var target_element_id: Elements = Elements.Water   # todo make this not hardcoded lmao
+var available_matches: Array = [Elements.Hydrogen, Elements.Lithium, Elements.Carbon] 
+
+signal player_element_changed(new_element_id)
+signal game_won() # or at least, the current goal has been reached
+
+func get_next_match_element_id() -> Elements:
+	var index = randi() % available_matches.size()
+	return available_matches[index]
+	
+func process_match_attempt(match_element_id: Elements) -> void:
+	var result_id = ElementCombiner_Script.combine_elements(current_element_id, match_element_id)
+
+	if result_id != NO_COMBINATION:
+		print("MATCH SUCCESS! ", Elements.keys()[current_element_id], " + ", Elements.keys()[match_element_id], " = ", Elements.keys()[result_id])
+		
+		current_element_id = result_id
+		
+		emit_signal("player_element_changed", current_element_id)
+		
+		if current_element_id == target_element_id:
+			emit_signal("game_won")
+		
+	else:
+		print("NO MATCH! ", Elements.keys()[current_element_id], " and ", Elements.keys()[match_element_id], " don't mix.")
