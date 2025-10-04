3830786e823dcda6e78247f4382ca68d94f4209b
James Wang <jwang477@mtroyal.ca>
2025-10-04 12:17:27 -0600

    Add ElementScript
    Script for data and creations. Call via 
    
    `var result_element = ElementScript.combine_elements(element_a, element_b)`

---
+++ scripts/ElementScript.gd
@@ -0,0 +1,82 @@
+extends Node
+
+
+
+
+
+# Enums in godotscript follow a 0 -> 1 -> 2 -> 3 default.
+# Hydrogen = 0, etc..
+# This is just shorthanding everything lol
+enum Elements {  
+    Hydrogen,
+	Helium,
+	Lithium,
+	Beryllium,
+	Boron,
+	Carbon,
+	Nitrogen,
+	Oxygen,
+	Fluorine,
+	Neon,
+	Sodium,
+	Magnesium,
+	Aluminum,
+	Silicon,
+	Phosphorus,
+	Sulfur,
+	Chlorine,
+	Argon,
+	Potassium,
+	Calcium,
+	Scandium,
+	Titanium,
+	Vanadium,
+	Chromium,
+	Manganese,
+	Iron,
+	Cobalt,
+	Nickel,
+	Copper,
+	Zinc,
+	# Combined elements goes here (30+...)
+	Water
+}  
+
+
+
+# All recipies lol
+const RECIPES: Dictionary = {
+    "0,7": Elements.Water, # 31
+}
+
+const NO_COMBINATION: int = -1
+func combine_elements(element_a: Elements, element_b: Elements) -> Elements:
+    var id_a: int = element_a
+    var id_b: int = element_b
+    
+# whatever order works
+    var key_parts: Array = [id_a, id_b]
+    key_parts.sort() 
+    
+    var combination_key: String = "%d,%d" % [key_parts[0], key_parts[1]]
+    
+    if RECIPES.has(combination_key):
+        return RECIPES[combination_key]
+    else:
+        return NO_COMBINATION
+
+
+#
+func _ready():
+    var result_water = combine_elements(Elements.Hydrogen, Elements.Oxygen)
+    print("Hydrogen + Oxygen = ", Elements.keys()[result_water])
+
+    var result_water_reverse = combine_elements(Elements.Oxygen, Elements.Hydrogen)
+    print("Oxygen + Hydrogen = ", Elements.keys()[result_water_reverse])
+
+    var result_none = combine_elements(Elements.Helium, Elements.Neon)
+    if result_none == NO_COMBINATION:
+        print("Break")
+    
+
+# Usage: var result_element = ElementScript.combine_elements(element_a, element_b)
