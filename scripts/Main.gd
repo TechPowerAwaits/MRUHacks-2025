fd53584260aeec2054c63fc3e1371e309c55103f
Muhammad Abdullah <atron1792@gmail.com>
2025-10-04 22:33:04 -0600

    hell

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -7,7 +7,9 @@ var element_card = preload("res://scripts/ElementCard.gd").new()
 @onready var ui = $UI 
 
 func _ready():
-	game_manager.combination_rejected.connect(ui._on_combination_rejected)
+	#game_manager.combination_rejected.connect(ui._on_combination_rejected)
+	game_manager.combination_accepted.connect(_on_gamemanager_combination_accepted)
+	#game_manager.combination_accepted.connect(signal: StringName, callable: Callable, flags: int = 0)
 
 func _on_tinder_accept() -> void:
 	$AcceptedSFX.play()
@@ -23,23 +25,21 @@ func _on_tinder_elm_request() -> void:
 	var elementID = game_manager.get_next_match_element_id()
 	element_card.setup_card(elementID, $Tinder/Element, $Tinder/Title, $Tinder/Description)
 
-func _on_gamemanger_combonation_accept() -> void:
-	if ($CurrentCharacter.texture == "res://Art assets/poof effect.png"):
-		$CurrentCharacter.texture = "res://Art assets/poof effect.png"
-		$CurrentCharacter/Timertimer.set_deferred("wait_time", 0.25)
-		$CurrentCharacter/Timertimer.set_deferred("one_shot", true)
-		$CurrentCharacter.texture = "res://Art assets/Characters/water character.png"
-
-	elif ($CurrentCharacter.texture == "res://Art assets/Characters/water character.png"):
-		$CurrentCharacter.texture = "res://Art assets/poof effect.png"
-		$CurrentCharacter/Timertimer.set_deferred("wait_time", 0.25)
-		$CurrentCharacter/Timertimer.set_deferred("one_shot", true)
-		$CurrentCharacter.texture = "res://Art assets/Characters/carbonate.png"
-	
-	elif ($CurrentCharacter.texture == "res://Art assets/Characters/carbonate.png"):
-		$CurrentCharacter.texture = "res://Art assets/poof effect.png"
-		$CurrentCharacter/Timertimer.set_deferred("wait_time", 0.25)
-		$CurrentCharacter/Timertimer.set_deferred("one_shot", true)
-		$CurrentCharacter.texture = "res://Art assets/Characters/calcium carbonate.png"
-
-	
+#func _on_gamemanager_combination_accepted() -> void:
+	#if ($CurrentCharacter.texture "res://Art assets/Characters/oxygen character.png"):
+		#print("check")
+		#$CurrentCharacter.texture = "res://Art assets/poof effect.png"
+		#await get_tree().create_timer(1.0).timeout
+		#$CurrentCharacter.texture = "res://Art assets/Characters/water character.png"
+#
+	#elif ($CurrentCharacter.texture == "res://Art assets/Characters/water character.png"):
+		#$CurrentCharacter.texture = "res://Art assets/poof effect.png"
+		#await get_tree().create_timer(1.0).timeout
+		#$CurrentCharacter.texture = "res://Art assets/Characters/carbonate.png"
+	#
+	#elif ($CurrentCharacter.texture == "res://Art assets/Characters/carbonate.png"):
+		#$CurrentCharacter.texture = "res://Art assets/poof effect.png"
+		#await get_tree().create_timer(1.0).timeout
+		#$CurrentCharacter.texture = "res://Art assets/Characters/calcium carbonate.png"
+#
+	#
