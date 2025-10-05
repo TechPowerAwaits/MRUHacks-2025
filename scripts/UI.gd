c7cdff3cee77c0b04ce5c7df7183c0ce321ea3e3
Jimmyking <Jimmyking999999@gmail.com>
2025-10-05 00:36:26 -0600

    Change goal1.png to actually be water, increase size of info box

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 	$GoalMenu.set_deferred("visible", true)
 	$NotQuiteMenu.set_deferred("visible", false)
 
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Toggle Menu"):
 		$PauseMenu.visible = !$PauseMenu.visible
@@ -23,12 +24,14 @@ func _on_resume_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+	
+	
 
 func _on_goal_button_pressed() -> void:
 	$GoalMenu.set_deferred("visible", true)
 
 func _on_goal_exit_pressed() -> void:	
-	$GoalMenu.set_deferred("visible", false)
+	$NotQuiteMenu.set_deferred("visible", false)
 
 func _on_combination_rejected(rejection_text: String) -> void:
 	$NotQuiteMenu/TextureRect/Description.text = rejection_text
