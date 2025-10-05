ad27a1fcf5101a6f2f11879c64972d2e1ffb9ff7
Richard Johnston <techpowerawaits@outlook.com>
2025-10-05 02:25:10 -0600

    Add comic 2

--- scripts/comic.gd
+++ scripts/comic.gd
@@ -1,4 +1,16 @@
 extends AnimationPlayer
 
+var pgNum: int = 1
+
 func _ready() -> void:
 	play("Comic")
+
+
+func _on_animation_finished(_anim_name: StringName) -> void:
+	print("Hi")
+	match pgNum:
+		1:
+			pgNum += 1
+			get_tree().change_scene_to_file("res://scenes/comic2.tscn")
+		2:
+			get_tree().change_scene_to_file("res://scenes/main.tscn")
